81854524f546d8a52d1fd33a44f1ddd2ea604d44
Lou TIGROUDJA <lou.tigroudja07@gmail.com>
2022-01-04 14:41:43 +0100

    Fin de la première partie

--- server.js
+++ server.js
@@ -6,11 +6,4 @@ app.get('/ping', (_request, response) => {
     response.json('pong');
 });
 
-const port = getRandomPort(3000, 4000);
-console.log(port);
-
-app.listen(port);
-
-function getRandomPort(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+app.listen(3000);
